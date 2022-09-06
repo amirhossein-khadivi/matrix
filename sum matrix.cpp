a3d9eb808582c7b40e96552471dd7b5184bdbbed
Amirhossein <khadivi.ah@yahoo.com>
2022-09-06 15:06:39 +0430

    sum

---
+++ sum matrix.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float A[3][4];
+	float sum=0;
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<4;j++)
+		{
+			cin >> [i][j];
+			sum = [i][j] + sum;
+		}
+		cout << sum;
+	} 
+	return 0;
+}
