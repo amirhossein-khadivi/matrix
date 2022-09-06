5c32a2dbf5773a5d031d1336f2d3979a7a0e7826
Amirhossein <khadivi.ah@yahoo.com>
2022-09-06 15:08:09 +0430

    triangular

---
+++ triangular matrix.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int A[5][5] = 0;
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			[i][j] = 1;
+		}
+	}
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			cout << a[i][j];
+		}
+    }
+	return 0;
+}
